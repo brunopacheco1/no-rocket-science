b1f81238def3f587d7fdc34bf203180c8482b45a
Bruno Pacheco <brunopacheco1@yahoo.com>
2022-03-06 15:52:31 +0100

    Read mission-000 file.

--- mission-000.cbl
+++ mission-000.cbl
@@ -9,27 +9,43 @@
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
            SELECT MISSIONFILE ASSIGN TO "mission-000.dat"
-            FILE STATUS IS FILE-CHECK-KEY
             ORGANIZATION IS LINE SEQUENTIAL.
                      
        DATA DIVISION.
        FILE SECTION.
        FD  MISSIONFILE.
+       01  MESSAGE-DETAILS.
+           88 ENDOFFILE VALUE HIGH-VALUES.
+           02 MESSAGE-TEXT PIC X(64).
 
        WORKING-STORAGE SECTION.
        01  WS-WORK-AREAS.
-           05 FILE-CHECK-KEY    PIC X(2).
+           05 PORT-NUMBER PIC 9(5).
 
        PROCEDURE DIVISION.
        
        0000-START.
-           PERFORM 0001-READ-MISSIONFILE.
+           PERFORM 0001-READ-FILE.
            STOP RUN.
        0001-END.
 
-       0001-READ-MISSIONFILE.
+       0001-READ-FILE.
            DISPLAY "Reading mission-000.dat...".
+           OPEN INPUT MISSIONFILE.
+           PERFORM 0002-READ-LINE UNTIL ENDOFFILE.
+           CLOSE MISSIONFILE.
            DISPLAY "Finished.".
        0001-END.
+
+       0002-READ-LINE.
+           PERFORM 0003-PROCESS-MESSAGE.
+           READ MISSIONFILE
+            AT END SET ENDOFFILE TO TRUE
+            END-READ.
+       0002-END.
+
+       0003-PROCESS-MESSAGE.
+           DISPLAY MESSAGE-TEXT.
+       0003-END.
 
        END PROGRAM MISSION-000.
